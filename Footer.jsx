8bcd8f98a65d153b3b542791fe8efdf279e6421e
dhanushch18 <86602109+dhanushch18@users.noreply.github.com>
2021-06-28 16:30:36 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+  return (
+    <footer>
+    <p>Copyright by ShapeAI @{ new Date().getFullyear} </p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return (
+      <div class="note">
+        <h1>Javascript and React.js</h1>
+        <p>The Bootcamp was nice and it was really interesting. </p>
+      </div>
+  );
+
+}
+
+export default Info;
